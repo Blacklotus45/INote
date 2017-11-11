4f01adb1d5dea0648abbf3c925dbf8dfc661e3e6
Berhan <bcgundogdu@gmail.com>
2017-11-11 19:15:02 +0000

    Root directed to notes_index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  root 'welcome#index'
+  root 'notes#index'
   
   resources :notes
   # The priority is based upon order of creation: first created -> highest priority.
