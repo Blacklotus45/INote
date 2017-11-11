34e48ec55545149b52e3bc0dc7d0d495171c01eb
Alperen Boyacı <alperen.boyaciii@gmail.com>
2017-11-11 13:27:46 +0000

    Proje not create edilebilir hale getirildi.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,9 +7,18 @@ class NotesController < ApplicationController
     end
     
     def new
+        @note = Note.new
     end
     
     def create
+        @note = Note.new(note_params)
+        
+        if @note.save
+            redirect_to @note 
+        else
+            render 'new'
+        end
+        
     end
     
     def edit
@@ -24,5 +33,6 @@ class NotesController < ApplicationController
     private
         
         def note_params
+            params.require(:note).permit(:title, :content)
         end
 end
